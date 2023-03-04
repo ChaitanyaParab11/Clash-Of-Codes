e140778330a7739289134b3644c60f10c2aa28f6
Chaitanya Parab <97729538+ChaitanyaParab11@users.noreply.github.com>
2023-03-04 18:30:51 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from tensorflow import keras
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 from tensorflow import keras
@@ -44,21 +44,26 @@ test_generator = test_datagen.flow_from_directory(test_dir,
 
 # define the model architecture with dropout regularization
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.2))
-model.add(Conv2D(64, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.3))
-model.add(Flatten())
-model.add(Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(0.001)))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
 
-# compile the model with a lower learning rate
-model.compile(loss='binary_crossentropy', 
-              optimizer=tf.keras.optimizers.Adam(lr=0.0001), 
-              metrics=['accuracy'])
+model.add(Conv2D(16, (3, 3), padding='same', input_shape=(img_width, img_height, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 # add early stopping to prevent overfitting
 early_stopping = EarlyStopping(monitor='val_loss', patience=5)
@@ -66,7 +71,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 # train the model with data augmentation and early stopping
 history = model.fit(train_generator, 
                     steps_per_epoch=len(train_generator), 
-                    epochs=50, 
+                    epochs=10, 
                     validation_data=valid_generator, 
                     validation_steps=len(valid_generator), 
                     callbacks=[early_stopping])
@@ -74,3 +79,5 @@ history = model.fit(train_generator,
 # evaluate the model on the test data
 test_loss, test_acc = model.evaluate(test_generator, steps=len(test_generator))
 print('Test accuracy:', test_acc)
+
+model.save('face_detection.model')
